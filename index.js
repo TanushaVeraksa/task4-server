550a53489596b2c87266eb27a082fb82c7517b85
TanushaVeraksa <veraksa161@gmail.com>
2023-08-16 02:24:33 +0300

    move files

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+const express = require("express");
+const PORT = process.env.PORT || 5000;
+const sequelize = require("./db");
+const router = require('./routes/index')
+const errorHandler = require('./middleware/ErrorMiddleware')
+const cors = require("cors");
+ 
+const app = express();
+
+app.use(cors());
+app.use(express.json())
+
+app.use('/api', router);
+app.use(errorHandler);
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
+    } catch(e){
+        throw new Error(e)
+    }
+}
+
+
+start()
