0290c5e25c305ead0f9076c8aa4d95d0d042a16f
TanushaVeraksa <veraksa161@gmail.com>
2023-08-16 01:28:53 +0300

    change index

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+// require('dotenv').config();
 const express = require("express");
 const PORT = process.env.PORT || 5000;
-const sequelize = require("./db");
-const router = require('./routes/index')
-const errorHandler = require('./middleware/ErrorMiddleware')
-const cors = require("cors");
+// const sequelize = require("./db");
+// const router = require('./routes/index')
+// const errorHandler = require('./middleware/ErrorMiddleware')
+// const cors = require("cors");
  
 const app = express();
 
@@ -16,8 +16,8 @@ app.use(errorHandler);
 
 const start = async () => {
     try {
-        await sequelize.authenticate();
-        await sequelize.sync();
+        // await sequelize.authenticate();
+        // await sequelize.sync();
         app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
     } catch(e){
         throw new Error(e)
